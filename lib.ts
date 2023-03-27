50d9cc4763c8ec97086dc02c0db5801eb1954ea8
grant0417 <grantgurvis@gmail.com>
2023-03-27 14:20:12 -0700

    Initial Commit

---
+++ lib.ts
@@ -0,0 +1,35 @@
+export type FilePath = string;
+export type Cache = FilePath | Cache[];
+
+export type StepFn = () => void | Promise<void> | number | Promise<number>;
+
+export type Step =
+  | {
+    name: string;
+    run: string | StepFn;
+  }
+  | StepFn
+  | string;
+
+export type JobOptions = {
+  name?: string;
+  image: string;
+  env?: Record<string, string>;
+  steps: Step[];
+};
+
+export class Job {
+  options: JobOptions;
+
+  constructor(options: JobOptions) {
+    this.options = options;
+  }
+}
+
+export class Pipeline {
+  jobs: Job[];
+
+  constructor(jobs: Job[]) {
+    this.jobs = jobs;
+  }
+}
