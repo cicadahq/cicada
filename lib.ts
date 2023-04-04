68275b97b8f4bd4768a6bab05a22306e1ea82489
Grant G <grant0417@users.noreply.github.com>
2023-04-04 14:02:27 -0700

    Update types for v0.1.25

--- lib.ts
+++ lib.ts
@@ -4,9 +4,27 @@
 export type FilePath = string;
 
 /**
+ * Options for a cached directory
+ */
+export type CacheDirectoryOptions = {
+  path: FilePath;
+  /**
+   * Directories default to `shared`
+   *
+   * - `shared` - can be used concurrently by multiple writers
+   * - `private` - creates a new cache if there are multiple writers
+   * - `locked` - pauses the second writer until the first one releases the cache
+   */
+  sharing?: "shared" | "private" | "locked";
+};
+
+/**
  * A directory to cache, which can be a single file path or an array of file paths.
  */
-export type CacheDir = FilePath | FilePath[];
+export type CacheDirectories = (
+  | FilePath
+  | CacheDirectoryOptions
+)[];
 
 /**
  * A step function that can return void or a number and can be synchronous or asynchronous.
@@ -20,25 +38,25 @@ export type StepFn = () => void | Promise<void> | number | Promise<number>;
 export type Step =
   | {
     /**
-     * The name of the step.
-     */
-    name: string;
-    /**
      * The command to run as a string or a step function.
      */
     run: string | StepFn;
+    /**
+     * The name of the step.
+     */
+    name?: string;
     /**
      * Cache directories, these will be mounted as docker volumes.
      *
      * If the path is absolute, it will be mounted as is, otherwise it will be mounted relative to the project root.
      * This will mount for all steps in the job.
      */
-    cacheDir?: CacheDir;
+    cacheDirectories?: CacheDirectories;
     /**
      * Disable caching for this step, this will cause the step to run every time, it may cause subsequent steps to run as well.
      * @default false
      */
-    cache?: boolean;
+    ignoreCache?: boolean;
     /**
      * Environment variables to set for this step.
      */
@@ -46,7 +64,7 @@ export type Step =
     /**
      * Secrets to expose for this step. They are accessed with `getSecret` or via the `/var/run/secrets` directory.
      */
-    secrets?: string[];
+    secrets?: Secret[];
     /**
      * Specify the working directory where this job should run
      */
@@ -84,7 +102,7 @@ export type JobOptions = {
    * If the path is absolute, it will be mounted as is, otherwise it will be mounted relative to the project root.
    * This will mount for all steps in the job.
    */
-  cacheDir?: CacheDir;
+  cacheDirectories?: CacheDirectories;
   /**
    * Specify the working directory where this job should run
    */
@@ -93,6 +111,12 @@ export type JobOptions = {
    * Require these jobs to run before the current job can be executed.
    */
   dependsOn?: Job[];
+  /**
+   * What to do if the job fails.
+   * - `ignore` - ignore the failure and continue the pipeline
+   * - `stop` - stop the pipeline
+   */
+  onFail?: "ignore" | "stop";
 };
 
 /**
@@ -125,27 +149,53 @@ export class Pipeline {
   constructor(public jobs: Job[]) {}
 }
 
-/**
- * Get a secret value from the secrets directory. The secret is only available during the job if it is specified in the job options.
- *
- * @param name The name of the secret
- * @returns The secret value
- */
-export async function getSecret(name: string): Promise<string> {
-  const secretPath = `/run/secrets/${name}`;
-  const secret = await Deno.readTextFile(secretPath);
-  return secret;
+function inJob() {
+  return Boolean(Deno.env.get("CICADA_JOB"));
 }
 
-/**
- * Get a secret value from the secrets directory. The secret is only available during the job if it is specified in the job options.
- * This is a synchronous version of `getSecret`.
- *
- * @param name The name of the secret
- * @returns The secret value
- */
-export function getSecretSync(name: string): string {
-  const secretPath = `/run/secrets/${name}`;
-  const secret = Deno.readTextFileSync(secretPath);
-  return secret;
+export class Secret {
+  /**
+   * Creates a new Secret instance.
+   * @param name - The name of the secret.
+   */
+  constructor(public name: string) {
+    if (inJob()) {
+      try {
+        Deno.statSync(`/run/secrets/${name}`);
+      } catch (_e) {
+        throw new Error(
+          `Secret \`${name}\` is not available in this job, make sure it is specified in the job options.`,
+        );
+      }
+    }
+  }
+
+  /**
+   * Get a secret value from the secrets directory. The secret is only available during the job if it is specified in the job options.
+   *
+   * @returns The secret value
+   */
+  async value(): Promise<string> {
+    if (!inJob()) {
+      throw new Error("Secrets are only available during a job.");
+    }
+    const secretPath = `/run/secrets/${this.name}`;
+    const secret = await Deno.readTextFile(secretPath);
+    return secret;
+  }
+
+  /**
+   * Get a secret value from the secrets directory. The secret is only available during the job if it is specified in the job options.
+   * This is a synchronous version of `getSecret`.
+   *
+   * @returns The secret value
+   */
+  valueSync(): string {
+    if (!inJob()) {
+      throw new Error("Secrets are only available during a job.");
+    }
+    const secretPath = `/run/secrets/${this.name}`;
+    const secret = Deno.readTextFileSync(secretPath);
+    return secret;
+  }
 }
