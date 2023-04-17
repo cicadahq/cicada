9f81d3413c2e37c86c14fd378bf7dced6a584d94
grant0417 <grantgurvis@gmail.com>
2023-04-17 11:03:47 -0700

    Add script to run all examples

---
+++ examples/run-all-examples.ts
@@ -0,0 +1,32 @@
+import { extname, join } from "https://deno.land/std@0.182.0/path/mod.ts";
+
+for (const examples of Deno.readDirSync(".")) {
+  if (!examples.isDirectory) continue;
+  const cicadaDir = join(examples.name, ".cicada");
+
+  for (const pipeline of Deno.readDirSync(cicadaDir)) {
+    if (extname(pipeline.name) !== ".ts") continue;
+    
+    const path = join(cicadaDir, pipeline.name);
+    console.log(path);
+
+    const child = new Deno.Command("cargo", {
+        args:[
+            "run",
+            "-p",
+            "cicada-cli",
+            "--",
+            "run",
+            "--cicada-dockerfile",
+            "../docker/bin.Dockerfile",
+            path
+        ],
+    }).spawn();
+
+    const status = await child.status;
+    if (!status.success) {
+      console.error(`Failed to run ${path}`);
+      Deno.exit(1);
+    }
+  }
+}
