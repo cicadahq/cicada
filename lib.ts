173143212e3444ea27169d54d3904d03aa5e1e6d
grant0417 <grantgurvis@gmail.com>
2023-04-18 16:27:49 -0700

    add deprecated warning

--- lib.ts
+++ lib.ts
@@ -1,1 +1,9 @@
+/**
+ * @deprecated lib.ts is deprecated, please use https://deno.land/x/cicada/mod.ts instead
+ */
+
 export * from "https://deno.land/x/cicada/mod.ts";
+
+console.error(
+  "lib.ts is deprecated, please use https://deno.land/x/cicada/mod.ts instead",
+);
